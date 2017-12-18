7c03295933bb911ae27296c3cc79a55b0f916676
BryanLX <517591210@qq.com>
2017-12-18 00:55:13 -0500

    start

---
+++ RollingBall/Assets/script/playerControler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerControler : MonoBehaviour {
+
+	public float speed;
+	private Rigidbody rb;
+
+	void Start(){
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate(){
+	
+		float horizonal = Input.GetAxis ("Horizontal");
+		float vertical = Input.GetAxis ("Vertical");
+
+		Vector3 move = new Vector3 (horizonal,0,vertical);
+
+
+		rb.AddForce (move*speed);
+	}
+
+}
